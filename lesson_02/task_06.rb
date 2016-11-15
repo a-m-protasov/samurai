340636b187e24195d45444356c9828e2a9c744d1
Alexey Protasov <a.m.protasov@gmail.com>
2016-11-15 05:54:13 +0300

    lesson02:task6

--- lesson_02/task_06.rb
+++ lesson_02/task_06.rb
@@ -6,3 +6,24 @@
 Также вывести итоговую сумму за каждый товар.
 Вычислить и вывести на экран итоговую сумму всех покупок в "корзине".
 =end
+
+cart = Hash.new
+sum = 0
+loop do
+  print "Введите название товара (или \"стоп\"): "
+  name = gets.chomp
+  break if name == "стоп"
+  print "цену: "
+  price = gets.chomp.to_f
+  print "кол-во: "
+  count = gets.chomp.to_f
+  
+  cart[name] = {"price" => price, "count" => count}  
+  # sum += price * count
+  # чтобы потренировать доступ к хешам, посчитаем сумму ниже
+end 
+
+cart.each {|name, hash| sum += hash["price"] * hash["count"]}
+
+puts cart
+puts "Итого: #{sum}"
